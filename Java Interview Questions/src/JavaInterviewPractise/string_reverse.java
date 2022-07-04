6fe8eb21aa3d9cd216fb31d98740f83f9fbd867f
Lenovo <Lenovo@LAPTOP-P3VHC49B>
2022-07-04 12:51:36 +0530

    Committing first interview question

--- Java Interview Questions/src/JavaInterviewPractise/string_reverse.java
+++ Java Interview Questions/src/JavaInterviewPractise/string_reverse.java
@@ -1,5 +1,15 @@
 package JavaInterviewPractise;
 
 public class string_reverse {
+	
+	public static void main(String[] args) {
+		String s = "Kiran Bhingare";
+		int len = s.length();
+		String rev = "";
+		for (int i=len-1; i>=0; i--) {
 
+			rev = rev + s.charAt(i);
+		}
+		System.out.println(rev);
+	}
 }
